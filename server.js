00fb20e87f26852ba748b11b002ba18a6cbb0ea9
Hobbes <43449480+ignaciuk@users.noreply.github.com>
2019-04-22 00:35:00 -0400

    updated package.json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var methodOverride = require('method-override');
 
-var PORT = process.env.PORT || 8086;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
