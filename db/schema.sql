f45d79bcb73d6d19636bef8a46f9bc10d826293a
Hobbes <43449480+ignaciuk@users.noreply.github.com>
2019-04-21 23:57:45 -0400

    updated DB files and package.json

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
-### Schema
+CREATE DATABASE burgers_db;
 
-CREATE DATABASE cat_db;
-USE cat_db;
+USE burgers_db;
 
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(100) NOT NULL,
+  devoured BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
 );
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,13 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Alaskan", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Iowan", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Conecticutian", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Masshole", FALSE);
