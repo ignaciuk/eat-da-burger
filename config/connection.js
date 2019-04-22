1e8d8a9ddd3e8c5dad299ff2af55744f7ea92c53
Hobbes <43449480+ignaciuk@users.noreply.github.com>
2019-04-22 07:51:46 -0400

    created JAWS_DB connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
+
+
 
 connection.connect(function(err) {
   if (err) {
